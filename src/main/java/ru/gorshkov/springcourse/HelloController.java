40f74f48f811bbe8267395af324f19f993f6b59c
gsn <gsn82@mail.ru>
2023-01-25 15:25:49 +0500

    3 lesson

--- src/main/java/ru/gorshkov/springcourse/HelloController.java
+++ src/main/java/ru/gorshkov/springcourse/HelloController.java
@@ -1,7 +1,14 @@
 package ru.gorshkov.springcourse;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+
+    @GetMapping("/helloWorld")
+    public String sayHello() {
+        return "hello_world";
+    }
 }

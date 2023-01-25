ecd4af560ab668ae46fb967ff0f2c526db9f9807
gsn <gsn82@mail.ru>
2023-01-25 20:11:50 +0500

    7 lesson

--- src/main/java/ru/gorshkov/springcourse/controllers/FirstController.java
+++ src/main/java/ru/gorshkov/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.gorshkov.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,10 +25,13 @@ public class FirstController {
 
 
     @GetMapping("/hello")
-    public String helloPage(@RequestParam("name") String name,
-                            @RequestParam("surname") String surname) {
+    public String helloPage(@RequestParam(value="name", required = false) String name,
+                            @RequestParam(value = "surname",required = false) String surname,
+                            Model model) {
 
-        System.out.println("Hello, " + name + "  " + surname);
+       // System.out.println("Hello, " + name + "  " + surname);
+
+        model.addAttribute("message", "Hello, " + name + "  " + surname);
 
         return "first/hello";
     }
@@ -36,4 +40,35 @@ public class FirstController {
     public String goodByePage() {
         return "first/goodbye";
     }
+
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a, @RequestParam("b") int b,
+                             @RequestParam("action") String action, Model model) {
+
+        double result;
+
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "division":
+                result = a / (double) b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        model.addAttribute("result", result);
+
+        return "first/calculator";
+    }
+
 }
